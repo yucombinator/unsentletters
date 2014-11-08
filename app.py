efdf58ffd11aece99d6341ca1b447947938cf05d
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-08 01:23:22 -0500

    show date

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template
 import praw
 import os
+import datetime
+import humanize
 
 app = Flask(__name__)
 
@@ -11,13 +13,12 @@ def getPost():
 @app.route("/")
 def home():
     post = getPost()
-    #while(len(post)==0):
-    #    post = getPost()
-    app.logger.debug(post)
-    app.logger.debug(post.title)
-    app.logger.debug(post.selftext)
 
-    return render_template('letter.html', title = post.title, text=post.selftext)
+    #app.logger.debug(post )
+    #app.logger.debug(post.title)
+    #app.logger.debug(post.selftext)
+    postedTime = humanize.naturaldate(datetime.datetime.fromtimestamp(post.created))
+    return render_template('letter.html', post = post, time = postedTime)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
