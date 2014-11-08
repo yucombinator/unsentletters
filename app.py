596557855c062610aef45aa135615a7aedd78c2b
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-08 03:33:48 -0500

    sentiment analysis + styling

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import praw
 import os
 import datetime
 import humanize
+import time
+import random
 from textblob import TextBlob
 
 app = Flask(__name__)
 
 r = praw.Reddit(user_agent='unsentletters/1.0 by /u/icechen1')
+def generateRandomDate():
+    start = 1301616000 #1st april 2011
+    end = int(time.time())
+    random_time =  random.randrange(1301616000, end)
+    return random_time
+
 def getPost():
     return r.get_random_submission(subreddit='unsentletters')
 def sentimentAnalysis(text):
@@ -19,7 +27,7 @@ def home():
     post = getPost()
     sentiment = sentimentAnalysis(post.selftext)
     app.logger.debug(sentiment)
-    #app.logger.debug(post.title)
+    app.logger.debug(generateRandomDate())
     #app.logger.debug(post.selftext)
     postedTime = humanize.naturaldate(datetime.datetime.fromtimestamp(post.created))
     return render_template('letter.html', post = post, time = postedTime, sentiment = sentiment)
