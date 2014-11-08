606454533a7959c8310706356376bfd7c69c90f4
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-08 03:02:50 -0500

    sentiment analysis + styling

--- app.py
+++ app.py
@@ -3,22 +3,26 @@ import praw
 import os
 import datetime
 import humanize
+from textblob import TextBlob
 
 app = Flask(__name__)
 
 r = praw.Reddit(user_agent='unsentletters/1.0 by /u/icechen1')
 def getPost():
     return r.get_random_submission(subreddit='unsentletters')
+def sentimentAnalysis(text):
+    blob = TextBlob(text)
+    return blob.sentiment
 
 @app.route("/")
 def home():
     post = getPost()
-
-    #app.logger.debug(post )
+    sentiment = sentimentAnalysis(post.selftext)
+    app.logger.debug(sentiment)
     #app.logger.debug(post.title)
     #app.logger.debug(post.selftext)
     postedTime = humanize.naturaldate(datetime.datetime.fromtimestamp(post.created))
-    return render_template('letter.html', post = post, time = postedTime)
+    return render_template('letter.html', post = post, time = postedTime, sentiment = sentiment)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
