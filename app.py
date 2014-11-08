26c3c4da6cefc36d459361bdd42f355b9d6709b3
Yu Chen Hou <houyuchen66@gmail.com>
2014-11-07 21:42:29 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 import praw
 
-app = Flask(__name__)
+app = Flask(__name__,port=int(os.environ.get("PORT", 5000)))
 
 r = praw.Reddit(user_agent='unsentletters/1.0 by /u/icechen1')
 def getPost():
